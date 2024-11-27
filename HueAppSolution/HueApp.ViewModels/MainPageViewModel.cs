a4d0ba67f9d5946ff2540d513bb74f8359499d09
DaanF1 <daanstander@hotmail.nl>
2024-11-27 12:22:54 +0100

    LoginScreen Bindings added
    Still an error

--- HueAppSolution/HueApp.ViewModels/MainPageViewModel.cs
+++ HueAppSolution/HueApp.ViewModels/MainPageViewModel.cs
@@ -1,13 +1,71 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HueApp.Domain.Clients;
 namespace HueApp.ViewModels
 {
     // All the code in this file is included in all platforms.
-    public class MainPageViewModel
+    public partial class MainPageViewModel : ObservableObject
     {
-        IPreferences preferences;
-        public MainPageViewModel(IPreferences preferences) 
+        private IPhilipsHueApiClient client;
+        private IPreferences preferences;
+        public MainPageViewModel(IPreferences preferences, IPhilipsHueApiClient client) 
         {
             this.preferences = preferences;
+            this.client = client;
         }
 
+        [ObservableProperty]
+        private string entryUsername;
+
+        [ObservableProperty]
+        private bool labelBridgeEnabled;
+
+        [ObservableProperty]
+        private bool entryBridgeEnabled;
+
+        [ObservableProperty]
+        private string entryBridgeText;
+
+        [ObservableProperty]
+        private bool labelPortEnabled;
+
+        [ObservableProperty]
+        private bool entryPortEnabled;
+
+        [ObservableProperty]
+        private string entryPortText;
+
+        private bool CheckedState = true;
+
+        [RelayCommand]
+        public async Task CheckBoxLocalHostChanged()
+        {
+            CheckedState = !CheckedState;
+            if (CheckedState)
+            {
+                CheckedState = !CheckedState;
+                entryBridgeEnabled = CheckedState;
+                labelBridgeEnabled = CheckedState;
+                entryBridgeText = "";
+                entryPortEnabled = !CheckedState;
+                labelPortEnabled = !CheckedState;
+            }
+            else
+            {
+                CheckedState = !CheckedState;
+                entryBridgeEnabled = CheckedState;
+                labelBridgeEnabled = CheckedState;
+                entryPortEnabled = !CheckedState;
+                labelPortEnabled = !CheckedState;
+                entryPortText = "";
+            }
+        }
+
+        [RelayCommand]
+        public async Task ButtonSubmitClicked()
+        {
+            string username = entryUsername;
+
+        }
     }
 }
--- HueAppSolution/HueApp/MainPage.xaml.cs
+++ HueAppSolution/HueApp/MainPage.xaml.cs
@@ -2,51 +2,16 @@
 using HueApp.Domain.Clients;
 using HueApp.Domain.Models.PhilipsLight;
 using HueApp.Infrastructure.HueApi;
+using HueApp.ViewModels;
 
 namespace HueApp
 {
     public partial class MainPage : ContentPage
     {
-        public bool CheckedState = true;
-
-        public MainPage()
+        public MainPage(MainPageViewModel mainPageViewModel)
         {
             InitializeComponent();
-        }
-
-        private void MainPageLoaded(object sender, EventArgs e)
-        {
-
-        }
-
-        private void CheckBoxIsLocalHostChanged(object sender, CheckedChangedEventArgs e)
-        {
-            if (CheckBoxIsLocalhost.IsChecked)
-            {
-                CheckedState = !CheckedState;
-                EntryBridge.IsEnabled = CheckedState;
-                LabelBridge.IsEnabled = CheckedState;
-                EntryBridge.Text = "";
-                EntryPort.IsEnabled = !CheckedState;
-                LabelPort.IsEnabled = !CheckedState;
-            } else
-            {
-                CheckedState = !CheckedState;
-                EntryBridge.IsEnabled = CheckedState;
-                LabelBridge.IsEnabled = CheckedState;
-                EntryPort.IsEnabled = !CheckedState;
-                LabelPort.IsEnabled = !CheckedState;
-                EntryPort.Text = "";
-            }
-        }
-
-        private async void ButtonSubmitClicked(object sender, EventArgs e)
-        {
-            //todo will be changed when MVVM gets implemented.
-            PhilipsHueApiClient client = new PhilipsHueApiClient(new HttpClient());
-
-            Dictionary<string, Light> lights = await client.GetLightsAsync();
-            //todo sla de lijst met lampen ergens op.
+            BindingContext = mainPageViewModel;
         }
     }
 }
