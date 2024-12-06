6c8b5c62338ef22e01f81dea392f083cf45dbf8a
DaanF1 <daanstander@hotmail.nl>
2024-12-06 16:39:22 +0100

    Changed async voids to async Task
    And added comments to the tests

--- HueAppSolution/HueApp.DomainTests/AppLightTest.cs
+++ HueAppSolution/HueApp.DomainTests/AppLightTest.cs
@@ -15,38 +15,64 @@ namespace HueApp.DomainTests
         [TestInitialize]
         public void TestInitialize()
         {
-            // Initialize mock client, test light, and state
+            // Initialize Mock Client, Test light, and State
             mockClient = new Mock<IPhilipsHueApiClient>();
             testState = new State { on = true, bri = 255, hue = 50000, sat = 200 };
             testLight = new Light { name = "Test Light", state = testState };
         }
 
         [TestMethod]
-        public async void AppLight_ShouldBeCorrectlySavedInLightClass_WhenReceievedFromApi()
+        public async Task AppLight_ShouldBeCorrectlySavedInLightClass_WhenReceievedFromApi()
         {
+            Console.WriteLine("Started test: AppLight_ShouldBeCorrectlySavedInLightClass_WhenReceievedFromApi"); // Log Start of Test
+            // Create Test Light to use for comparing with the result
             var testResult = new Dictionary<string, Light>()
             {
                 { "1", testLight }
             };
+
+            Console.WriteLine("-Expected Results-"); // Log Header of Expected Results
+            for (int i = 1; i < testResult.Count + 1; i++)
+            {
+                Console.WriteLine($"Expected Light Name: {testResult[i.ToString()].name}"); // Log a Single Expected Result
+            }
+            
+            // Setup Mock
             mockClient.Setup(mock => mock.GetLightsAsync(It.IsAny<string>())).ReturnsAsync(testResult);
 
+            // Use Function
             var result = await mockClient.Object.GetLightsAsync("");
 
+            Console.WriteLine("-Results-"); // Log Header of Results
+            for (var i = 1; i < result.Count + 1; i++)
+            {
+                Console.WriteLine($"Name: {result[i.ToString()].name}"); // Log a Single Result
+            }
+
+            // Assert Result and compare to expected data
             Assert.IsNotNull(result);
             Assert.AreEqual(testLight, result["1"]);
+            Console.WriteLine("Finished test: AppLight_ShouldBeCorrectlySavedInLightClass_WhenReceievedFromApi"); // Log End of Test
         }
 
         [TestMethod]
-        public async void Api_ShouldReturnUsername_WhenLinkIsSuccesful()
+        public async Task Api_ShouldReturnUsername_WhenLinkIsSuccesful()
         {
+            Console.WriteLine("Started test: Api_ShouldReturnUsername_WhenLinkIsSuccesful"); // Log Start of Test
+            string testname = "username";
+            // Setup Mock
             mockClient.Setup(client => client.Link(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
-                .ReturnsAsync("username");
+                .ReturnsAsync(testname);
+            Console.WriteLine($"Expected Name From Link: {testname}D"); // Log Expected Name
 
+            // Use Function
             var usernameFromLink = await mockClient.Object.Link("Url", "peter", "windowsPC");
 
-            //check result and verify method call
-            Assert.AreEqual("username", usernameFromLink);
+            // Assert Result and Verify Method Call
+            Console.WriteLine($"Username From Link: {usernameFromLink}"); // Log Gotten Name
+            Assert.AreEqual(testname, usernameFromLink);
             mockClient.Verify(client => client.Link(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+            Console.WriteLine("Finished test: Api_ShouldReturnUsername_WhenLinkIsSuccesful"); // Log End of Test
         }
     }
 }
