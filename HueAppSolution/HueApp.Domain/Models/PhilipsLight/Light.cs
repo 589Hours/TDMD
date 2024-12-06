059ca19daa58b8e907153028ce8ef6a2ad6fc5fa
589Hours <rik.vanham@ziggo.nl>
2024-12-06 14:46:35 +0100

    start on test

--- HueAppSolution/HueApp.Domain/Models/PhilipsLight/Light.cs
+++ HueAppSolution/HueApp.Domain/Models/PhilipsLight/Light.cs
@@ -4,12 +4,7 @@ namespace HueApp.Domain.Models.PhilipsLight
     public class Light
     {
         public State state { get; set; }
-        public string type { get; set; }
         public string name { get; set; }
-        public string modelid { get; set; }
-        public string swversion { get; set; }
-        public string uniqueid { get; set; }
-        public Pointsymbol pointsymbol { get; set; }
 
         public override string ToString()
         {
--- HueAppSolution/HueApp.DomainTests/AppLightTest.cs
+++ HueAppSolution/HueApp.DomainTests/AppLightTest.cs
@@ -1,11 +1,34 @@
-﻿namespace HueApp.DomainTests
+﻿using HueApp.Domain.Clients;
+using HueApp.Domain.Models.PhilipsLight;
+using HueApp.Infrastructure.HueApi;
+using Moq;
+
+namespace HueApp.DomainTests
 {
     [TestClass]
-    public sealed class Test1
+    public sealed class AppLightTest
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void AppLight_ShouldBeCorrectlySavedInLightClass_WhenReceievedFromApi()
         {
+            var httpMock = new Mock<IPhilipsHueApiClient>();
+            var testLight = new Light() { name = "Test", state = new State()
+            {
+                on = false,
+                hue = 5000,
+                sat = 254,
+                bri = 254
+            } };
+
+            var testResult = new Dictionary<string, Light>()
+            {
+                { "1", testLight }
+            };
+            httpMock.Setup(mock => mock.GetLightsAsync("")).ReturnsAsync(testResult);
+            PhilipsHueApiClient = new 
+
+            Assert.AreEqual(testLight, httpMock.Object);
         }
     }
 }
